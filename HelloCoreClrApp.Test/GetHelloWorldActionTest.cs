1d71dd8869ae9a860deeaf16b7ce6f65edc0d5a0
Jens Peters <j.peters@nl.ccv.eu>
2016-03-10 11:59:04 +0100

    Add some unit tests for playing ..

--- HelloCoreClrApp.Test/GetHelloWorldActionTest.cs
+++ HelloCoreClrApp.Test/GetHelloWorldActionTest.cs
@@ -11,5 +11,29 @@ namespace HelloWorldApp
             var result = sut.Execute("World");
             Assert.Equal("Hello World!", result.Name);
         }
+        
+        [Fact]
+        public void ExecuteTest2()
+        {
+            var sut = new GetHelloWorldAction();
+            var result = sut.Execute("World1");
+            Assert.Equal("Hello World1!", result.Name);
+        }
+        
+        [Fact(Skip="No fun")]
+        public void ExecuteTest3()
+        {
+            var sut = new GetHelloWorldAction();
+            var result = sut.Execute("World3");
+            Assert.Equal("Hello World3!", result.Name);
+        }
+        
+        [Fact]
+        public void ExecuteTest4()
+        {
+            var sut = new GetHelloWorldAction();
+            var result = sut.Execute("World5");
+            Assert.Equal("Hello World4!", result.Name);
+        }
     }
 }

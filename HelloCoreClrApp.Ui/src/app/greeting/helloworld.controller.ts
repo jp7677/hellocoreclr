91a589a396bb61fb477b2b9c8139f9260c0b016d
Jens Peters <jp7677@gmail.com>
2016-04-14 21:01:22 +0200

    Format toastr.

--- HelloCoreClrApp.Ui/src/app/greeting/helloworld.controller.ts
+++ HelloCoreClrApp.Ui/src/app/greeting/helloworld.controller.ts
@@ -11,6 +11,8 @@ namespace app.greeting {
         constructor(private $http: ng.IHttpService, private $log: ng.ILogService) {
             this.inputText = undefined;
             this.labelText = "";
+
+            this.SetToastrOptions();
         }
 
         public executeHelloWorld(): void  {
@@ -29,17 +31,26 @@ namespace app.greeting {
                     this.$log.info("Received http code " + status);
                     this.$log.info("Received data was: " + data.name);
 
+                    toastr.clear();
                     toastr.success("HTTP/" + status);
                     this.labelText = data.name;
                 })
                 .error((data: GetHelloWorldResponse, status: number) => {
                     this.$log.info("Received http code " + status);
-                    this.$log.warn("Oops... something went wrong");
+                    this.$log.warn("Oops... something went wrong.");
 
+                    toastr.clear();
                     toastr.warning("Oops... HTTP/" + status);
                     this.labelText = "";
                 });
         };
+
+        private SetToastrOptions(): void {
+            toastr.options.positionClass = "toast-bottom-right";
+            toastr.options.timeOut = 1500;
+            toastr.options.showDuration = 100;
+            toastr.options.hideDuration = 250;
+        }
     }
 
     let app = angular.module("app");

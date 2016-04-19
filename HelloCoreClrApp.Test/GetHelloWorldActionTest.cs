31dec0c4e747a7913872d1af0b949c568799367b
Jens Peters <jp7677@gmail.com>
2016-04-19 20:14:59 +0200

    Add some boundary tests. Add dnx watcher command to run tests on file save.

--- HelloCoreClrApp.Test/GetHelloWorldActionTest.cs
+++ HelloCoreClrApp.Test/GetHelloWorldActionTest.cs
@@ -11,5 +11,21 @@ namespace HelloWorldApp
             var result = sut.Execute("World");
             Assert.Equal("Hello World!", result.Name);
         }
+        
+        [Fact]
+        public void ExecuteTestWithNull()
+        {
+            var sut = new GetHelloWorldAction();
+            var result = sut.Execute(null);
+            Assert.Equal("Are you sure?", result.Name);
+        }
+        
+        [Fact]
+        public void ExecuteTestWithEmptyString()
+        {
+            var sut = new GetHelloWorldAction();
+            var result = sut.Execute("");
+            Assert.Equal("Are you sure?", result.Name);
+        }
     }
 }
--- HelloCoreClrApp/GetHelloWorldAction.cs
+++ HelloCoreClrApp/GetHelloWorldAction.cs
@@ -4,9 +4,13 @@ namespace HelloWorldApp
     {
         public GetHelloWorldResponse Execute(string name)
         {
+            string result = !string.IsNullOrEmpty(name) 
+                            ? string.Format("Hello {0}!", name)
+                            : "Are you sure?";
+            
             return new GetHelloWorldResponse
             {
-                Name = string.Format("Hello {0}!", name)
+                Name = result 
             };
         }
     }

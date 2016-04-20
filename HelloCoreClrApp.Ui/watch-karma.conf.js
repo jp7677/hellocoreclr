0451f49374084ea1a160a1576bc1a4332e2eef75
Jens Peters <jp7677@gmail.com>
2016-04-20 23:00:45 +0200

    Add watcher configurations for karma and CoreClr build.

---
+++ HelloCoreClrApp.Ui/watch-karma.conf.js
@@ -0,0 +1,18 @@
+module.exports = function (config) {
+  'use strict'
+
+  config.set({
+    frameworks: ['mocha', 'chai', 'sinon'],
+    files: [
+      {pattern: 'bower_components/jquery/dist/jquery.js', instrument: false},
+      {pattern: 'bower_components/toastr/toastr.js', instrument: false},
+      {pattern: 'bower_components/angular/angular.js', instrument: false},
+      {pattern: 'node_modules/angular-mocks/angular-mocks.js', instrument: false},
+      'src/app/**/*.js',
+      'test/stubs.js',
+      'test/**/*.spec.js'
+    ],
+
+    browsers: ['PhantomJS']
+  })
+}

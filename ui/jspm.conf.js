1c51266b2a85bf98de46413983ead3ef3d21a47d
Jens Peters <jp7677@gmail.com>
2016-09-13 21:57:17 +0200

    Restore karma tests. Found a way to set a different baseURL within the browser.

--- ui/jspm.conf.js
+++ ui/jspm.conf.js
@@ -1,20 +1,26 @@
 System.config({
-  baseURL: '/',
   defaultJSExtensions: true,
   transpiler: false,
   paths: {
-    'npm:*': '../jspm_packages/npm/*',
-    'github:*': '../jspm_packages/github/*'
+    'npm:*': 'jspm_packages/npm/*',
+    'github:*': 'jspm_packages/github/*'
   },
 
   map: {
     'angular': 'npm:angular@1.5.8',
     'angular-bootstrap': 'npm:angular-bootstrap@0.12.2',
+    'angular-mocks': 'github:angular/bower-angular-mocks@1.5.8',
     'angular-ui-router': 'npm:angular-ui-router@0.3.1',
     'bootstrap': 'npm:bootstrap@3.3.7',
+    'chai': 'npm:chai@3.5.0',
     'font-awesome': 'npm:font-awesome@4.6.3',
     'jquery': 'npm:jquery@3.1.0',
+    'mocha': 'npm:mocha@3.0.2',
+    'sinon': 'npm:sinon@1.17.5',
     'toastr': 'npm:toastr@2.1.2',
+    'github:angular/bower-angular-mocks@1.5.8': {
+      'angular': 'github:angular/bower-angular@1.5.8'
+    },
     'github:jspm/nodelibs-assert@0.1.0': {
       'assert': 'npm:assert@1.4.1'
     },
@@ -56,11 +62,50 @@ System.config({
       'isarray': 'npm:isarray@1.0.0',
       'process': 'github:jspm/nodelibs-process@0.1.2'
     },
+    'npm:chai@3.5.0': {
+      'assertion-error': 'npm:assertion-error@1.0.2',
+      'buffer': 'github:jspm/nodelibs-buffer@0.1.0',
+      'deep-eql': 'npm:deep-eql@0.1.3',
+      'process': 'github:jspm/nodelibs-process@0.1.2',
+      'systemjs-json': 'github:systemjs/plugin-json@0.1.2',
+      'type-detect': 'npm:type-detect@1.0.0'
+    },
+    'npm:debug@2.2.0': {
+      'ms': 'npm:ms@0.7.1'
+    },
+    'npm:deep-eql@0.1.3': {
+      'buffer': 'github:jspm/nodelibs-buffer@0.1.0',
+      'type-detect': 'npm:type-detect@0.1.1'
+    },
     'npm:font-awesome@4.6.3': {
       'css': 'github:systemjs/plugin-css@0.1.27'
     },
+    'npm:formatio@1.1.1': {
+      'process': 'github:jspm/nodelibs-process@0.1.2',
+      'samsam': 'npm:samsam@1.1.2'
+    },
     'npm:inherits@2.0.1': {
       'util': 'github:jspm/nodelibs-util@0.1.0'
+    },
+    'npm:lodash._baseassign@3.2.0': {
+      'lodash._basecopy': 'npm:lodash._basecopy@3.0.1',
+      'lodash.keys': 'npm:lodash.keys@3.1.2'
+    },
+    'npm:lodash.create@3.1.1': {
+      'lodash._baseassign': 'npm:lodash._baseassign@3.2.0',
+      'lodash._basecreate': 'npm:lodash._basecreate@3.0.3',
+      'lodash._isiterateecall': 'npm:lodash._isiterateecall@3.0.9'
+    },
+    'npm:lodash.keys@3.1.2': {
+      'lodash._getnative': 'npm:lodash._getnative@3.9.1',
+      'lodash.isarguments': 'npm:lodash.isarguments@3.1.0',
+      'lodash.isarray': 'npm:lodash.isarray@3.0.4'
+    },
+    'npm:mocha@3.0.2': {
+      'css': 'github:systemjs/plugin-css@0.1.27',
+      'debug': 'npm:debug@2.2.0',
+      'json3': 'npm:json3@3.3.2',
+      'lodash.create': 'npm:lodash.create@3.1.1'
     },
     'npm:path-browserify@0.0.0': {
       'process': 'github:jspm/nodelibs-process@0.1.2'
@@ -69,6 +114,13 @@ System.config({
       'assert': 'github:jspm/nodelibs-assert@0.1.0',
       'fs': 'github:jspm/nodelibs-fs@0.1.2',
       'vm': 'github:jspm/nodelibs-vm@0.1.0'
+    },
+    'npm:sinon@1.17.5': {
+      'formatio': 'npm:formatio@1.1.1',
+      'lolex': 'npm:lolex@1.3.2',
+      'process': 'github:jspm/nodelibs-process@0.1.2',
+      'samsam': 'npm:samsam@1.1.2',
+      'util': 'npm:util@0.10.3'
     },
     'npm:toastr@2.1.2': {
       'process': 'github:jspm/nodelibs-process@0.1.2'
--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -2,16 +2,27 @@ module.exports = function (config) {
   'use strict'
 
   config.set({
-    frameworks: ['mocha', 'chai', 'sinon'],
+    frameworks: ['systemjs', 'mocha', 'chai', 'sinon'],
+
     files: [
-      {pattern: 'bower_components/jquery/dist/jquery.js', instrument: false},
-      {pattern: 'bower_components/toastr/toastr.js', instrument: false},
-      {pattern: 'bower_components/angular/angular.js', instrument: false},
-      {pattern: 'node_modules/angular-mocks/angular-mocks.js', instrument: false},
       'src/app/**/*.js',
       'test/stubs.js',
+      'test/*.spec.js',
       'test/**/*.spec.js'
     ],
+
+    systemjs: {
+      configFile: 'jspm.conf.js',
+      serveFiles: [
+        'jspm_packages/**/*.js'
+      ],
+      config: {
+        paths: {
+          'systemjs': 'node_modules/systemjs/dist/system.js',
+          'system-polyfills': 'node_modules/systemjs/dist/system-polyfills.js'
+        }
+      }
+    },
 
     autoWatchBatchDelay: 10000,
     singleRun: true,

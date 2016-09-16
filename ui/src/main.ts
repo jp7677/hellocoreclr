cef5e2c35b4a76fc6a3748224c7d654ccaa8389e
Jens Peters <jp7677@gmail.com>
2016-09-16 23:10:00 +0200

    Do not default apiBaseUrl to localhost. That was needed to serve from file://, but we shouldn't do that anymore. Though still needs some adjustments to VS Code task and some gulp magic.

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -4,7 +4,7 @@ import { Config } from "./app/app.config";
 import { HelloWorldController } from "./app/greeting/helloworld.controller";
 
 const main = angular.module("app", ["ui.router", "ui.bootstrap"])
-                 .constant("apiBaseUrl", "http://localhost:5000/api/")
+                 .constant("apiBaseUrl", "/api/")
                  .config(Config)
                  .controller("HelloWorldController", HelloWorldController);
 

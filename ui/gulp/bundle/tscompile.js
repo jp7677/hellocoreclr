897228cfee73350692078ca1838e26a1b83fe4e9
Jens Peters <jp7677@gmail.com>
2016-09-23 10:00:19 +0200

    Do not compile typescript tests in production mode.

--- ui/gulp/bundle/tscompile.js
+++ ui/gulp/bundle/tscompile.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const iif = require('gulp-if')
+const filter = require('gulp-filter')
 const ts = require('gulp-typescript')
 const sourcemaps = require('gulp-sourcemaps')
 const util = require('gulp-util')
@@ -9,6 +10,7 @@ exports.dep = ['clean:js']
 exports.fn = function (gulp, paths, mode, done) {
   var tsProject = ts.createProject('tsconfig.json')
   var tsResult = tsProject.src()
+    .pipe(iif(mode.production, filter(['**/*.ts', '!**/*.spec.ts'])))
     .pipe(iif(!mode.production, sourcemaps.init()))
     .pipe(ts(tsProject))
 

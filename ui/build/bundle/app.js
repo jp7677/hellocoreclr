0b59d97befe89459b7dd2921fee1be2b49819695
Jens Peters <jp7677@gmail.com>
2016-10-12 16:50:41 +0200

    Use the correct entry point.

--- ui/build/bundle/app.js
+++ ui/build/bundle/app.js
@@ -9,7 +9,7 @@ const util = require('gulp-util')
 
 exports.dep = ['bundle:tscompile']
 exports.fn = function (gulp, paths, mode, done) {
-  return gulp.src(paths.src + './app.js', { base: '.' })
+  return gulp.src(paths.src + './main.js', { base: '.' })
     .pipe(!mode.production ? sourcemaps.init({loadMaps: true}) : util.noop())
     .pipe(jspm({selfExecutingBundle: true, minify: mode.production, mangle: mode.production, fileName: 'app-bundle'}))
     .pipe(mode.production ? hash({'format': '{hash}{ext}'}) : util.noop())

a441f87368aafed244855ce2f53266e29c8c2e10
Jens Peters <jp7677@gmail.com>
2016-10-13 22:43:47 +0200

    Copy SystemJs sourcemap when running in non production mode.

--- ui/build/bundle/js.js
+++ ui/build/bundle/js.js
@@ -4,7 +4,13 @@ const flatten = require('gulp-flatten')
 
 exports.dep = ['bundle:app']
 exports.fn = function (gulp, paths, mode, done) {
-  return gulp.src([paths.src + './jspm_packages/system.js', paths.src + './app-bundle*.js'], { base: '.' })
+  var files = [paths.src + './jspm_packages/system.js', paths.src + './app-bundle*.js']
+  if (!mode.production) {
+    files.push(paths.src + './jspm_packages/system.js.map')
+    files.push(paths.src + './jspm_packages/system.src.js')
+  }
+
+  return gulp.src(files, { base: '.' })
   .pipe(flatten())
   .pipe(gulp.dest(paths.wwwroot))
 }

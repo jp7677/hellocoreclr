b4654cdf021ce3153bebbe73c6d564feace57774
Jens Peters <jp7677@gmail.com>
2016-11-05 08:07:32 +0100

    Clarify comments.

--- ui/build/test/js.js
+++ ui/build/test/js.js
@@ -13,7 +13,8 @@ exports.fn = function (gulp, paths, mode, done) {
     // We are supposed to run i.c.w. watch/browsersync.
     // So regardless of the karma exit code we just tell gulp
     // that the test run finished sucessfully. In a CI scenario
-    // you would obviously propagate the exit code to gulp.
+    // you would obviously propagate the exit code to gulp for
+    // letting the CI pipeline know that tests have failed.
     done()
   })
 }

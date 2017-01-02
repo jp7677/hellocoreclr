b581da5ef4162e0ceabe592962c0164ce2f606b3
Jens Peters <jp7677@gmail.com>
2017-01-02 10:38:02 +0100

    Improve readability.

--- ui/test/e2e.spec.js
+++ ui/test/e2e.spec.js
@@ -1,11 +1,13 @@
 var mocha = require('mocha')
+var describe = mocha.describe
+var it = mocha.it
 var chai = require('chai')
 var chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
 var protractor = require('protractor')
 
-mocha.describe('End2end test suite', () => {
-  mocha.it('should load completely and have a title', (done) => {
+describe('End2end test suite', () => {
+  it('should load completely and have a title', (done) => {
     protractor.browser.loadAndWaitForAureliaPage('/')
     chai.expect(protractor.browser.getTitle()).to.eventually.be.equal('Hello World | Hello World')
       .notify(done)

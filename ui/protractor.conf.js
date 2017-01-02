9045a76bb59434e9eb2810b04654a5c993364695
Jens Peters <jp7677@gmail.com>
2017-01-02 07:50:07 +0100

    Run protractor on appveyor This hould fail now because we don't have a running web server. Add some protractor settings specific to appveyor, see https://github.com/FeodorFitsner/AppVeyor-Protractor-test-app/blob/master/protractor.conf.js#L5-L7.

--- ui/protractor.conf.js
+++ ui/protractor.conf.js
@@ -6,15 +6,22 @@ exports.config = {
   plugins: [{
     package: 'aurelia-protractor-plugin'
   }],
+
   capabilities: {
     'browserName': 'phantomjs',
     'phantomjs.binary.path': phantomjs.path
   },
   seleniumServerJar: './node_modules/selenium-jar/bin/selenium-server-standalone-2.52.0.jar',
+  localSeleniumStandaloneOpts: {
+    args: ['-Djna.nosys=true']
+  },
+
   specs: ['test/e2e.spec.js'],
+
   onPrepare: () => {
     protractor.ignoreSynchronization = true
   },
+
   mochaOpts: {
     reporter: 'spec',
     timeout: 6000

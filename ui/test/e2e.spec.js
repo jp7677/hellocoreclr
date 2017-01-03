4e348372aad4722102fc1cde68e79947e8d7497f
Jens Peters <jp7677@gmail.com>
2017-01-03 07:55:34 +0100

    Revert "Test if Ci fails on a broken e2e test."
    This reverts commit 571d76a547a949dd18774d0b64be037a8ee09572.

--- ui/test/e2e.spec.js
+++ ui/test/e2e.spec.js
@@ -9,7 +9,7 @@ var protractor = require('protractor')
 describe('End2end test suite', () => {
   it('should load completely and have a title', (done) => {
     protractor.browser.loadAndWaitForAureliaPage('/')
-    chai.expect(protractor.browser.getTitle()).to.eventually.be.equal('Hello World | Hello Worl1d')
+    chai.expect(protractor.browser.getTitle()).to.eventually.be.equal('Hello World | Hello World')
       .notify(done)
   })
 })

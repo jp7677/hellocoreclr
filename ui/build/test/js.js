2316f136a00fe2f169c6430b44efd1d9fa131952
Jens Peters <jp7677@gmail.com>
2017-01-03 15:45:42 +0100

    Use yargs to handle command line arguments.

--- ui/build/test/js.js
+++ ui/build/test/js.js
@@ -1,18 +1,13 @@
 'use strict'
 
-const argv = require('yargs').argv
 const path = require('path')
 
 exports.fn = function (gulp, paths, mode, done) {
   var karma = require('karma').Server
 
-  let reporters = argv.karmareporters === undefined
-    ? ['mocha', 'coverage', 'remap-coverage']
-    : argv.karmareporters
-
   karma.start({
     configFile: path.join(__dirname, '..', '..', 'karma.conf.js'),
-    reporters: reporters
+    reporters: mode.reporters
   }, function (exitCode) {
     done()
     if (!mode.watch) {
--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -1,12 +1,20 @@
 ﻿'use strict'
 
 const gulp = require('gulp')
-const argv = require('yargs').argv
+const argv = require('yargs')
+  .default('production', false)
+  .default('nobuild', false)
+  .default('karmareporters', ['mocha', 'coverage', 'remap-coverage'])
+  .argv
 const load = require('gulp-require-tasks')
 const run = require('run-sequence')
-const util = require('gulp-util')
 
-const mode = { production: !!util.env.production, watch: false }
+const mode = {
+  production: argv.production,
+  watch: argv._.includes('watch'),
+  reporters: argv.karmareporters
+}
+
 const paths = {
   wwwroot: './wwwroot/',
   src: './src/',
@@ -28,12 +36,11 @@ gulp.task('build', function (done) {
 gulp.task('default', ['build'])
 
 gulp.task('watch', function (done) {
-  mode.watch = true
   run('watch:all', done)
 })
 
 gulp.task('unit-tests', function (done) {
-  if (argv.nobuild === undefined) {
+  if (!argv.nobuild) {
     run('bundle:tscompile', 'test:js', done)
   } else {
     run('test:js', done)
@@ -41,7 +48,7 @@ gulp.task('unit-tests', function (done) {
 })
 
 gulp.task('e2e-tests', function (done) {
-  if (argv.nobuild === undefined) {
+  if (!argv.nobuild) {
     run('build', 'test:e2e', 'serve:stop', done)
   } else {
     run('test:e2e', 'serve:stop', done)

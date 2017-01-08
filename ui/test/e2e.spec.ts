8a626d7dbd89d6595db008558d1a31754e318385
Jens Peters <jp7677@gmail.com>
2017-01-08 21:10:37 +0100

    Improve e2e test.

--- ui/test/e2e.spec.ts
+++ ui/test/e2e.spec.ts
@@ -6,9 +6,22 @@ before(() => {
   chai.use(chaiAsPromised);
 });
 
-describe("End2end test suite (typescript)", () => {
+beforeEach(async() => {
+    await protractor.browser.loadAndWaitForAureliaPage("/");
+});
+
+describe("HelloWorld app", () => {
   it("should load completely and have a title", async() => {
-    protractor.browser.loadAndWaitForAureliaPage("/");
-    await chai.expect(protractor.browser.getTitle()).to.eventually.be.equal("Say Hello World! | Hello World");
+    return chai.expect(protractor.browser.getTitle()).to.eventually.be.equal("Say Hello World! | Hello World");
+  });
+
+  it("should navigate to greetings", async() => {
+    await protractor.element(protractor.by.css('a[href="#/greetings"]')).click();
+    // the sleep statement should be replace with
+    // await protractor.browser.waitForRouterComplete();
+    // but unfortunately we get bitten by
+    // https://github.com/aurelia/protractor-plugin/issues/2
+    await protractor.browser.sleep(1000);
+    return chai.expect(protractor.browser.getTitle()).to.eventually.be.equal("Greetings | Hello World");
   });
 });

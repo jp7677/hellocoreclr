78f8be18c8042305c401f1ac47e4ba5a3c2105f8
Jens Peters <jp7677@gmail.com>
2017-01-10 16:10:22 +0100

    Remove not-needed option. The aurelia plugin already takes care of this.

--- ui/protractor.conf.js
+++ ui/protractor.conf.js
@@ -1,4 +1,3 @@
-const protractor = require('protractor')
 const phantomjs = require('phantomjs-prebuilt')
 
 exports.config = {
@@ -21,10 +20,6 @@ exports.config = {
 
   baseUrl: 'http://localhost:3000',
   specs: ['test/e2e*.spec.js'],
-
-  onPrepare: () => {
-    protractor.ignoreSynchronization = true
-  },
 
   mochaOpts: {
     reporter: 'spec',

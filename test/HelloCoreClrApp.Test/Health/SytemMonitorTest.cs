2752647e0c7d9cae2b589647639f0059f3e78113
Jens Peters <jp7677@gmail.com>
2017-01-19 21:39:02 +0100

    Add basic unit test.

---
+++ test/HelloCoreClrApp.Test/Health/SytemMonitorTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FakeItEasy;
+using FluentAssertions;
+using HelloCoreClrApp.Health;
+using Xunit;
+
+namespace HelloCoreClrApp.Test.Health
+{
+    public class SytemMonitorTest
+    {
+        [Fact]
+        public async void RunTestAsync()
+        {
+            var testCts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+            var monitor = A.Fake<IMonitor>();
+            var cts = new CancellationTokenSource();
+            var sut = new SystemMonitor(new []{monitor});
+
+            var task = sut.Run(cts.Token);
+
+            await Task.Delay(TimeSpan.FromMilliseconds(100), testCts.Token);
+            cts.Cancel();
+            await Task.Delay(TimeSpan.FromMilliseconds(100), testCts.Token);
+
+            task.Status.Should().Be(TaskStatus.RanToCompletion);
+        }
+    }
+}

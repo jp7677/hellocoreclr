a9c351bd9fe533a73e6076c534dbb91de395dbb9
Jens Peters <jp7677@gmail.com>
2017-01-20 07:24:08 +0100

    Add basic test.

---
+++ test/HelloCoreClrApp.Test/WebApi/WebHostTaskTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using FakeItEasy;
+using HelloCoreClrApp.WebApi;
+using Microsoft.Extensions.Configuration;
+using SimpleInjector;
+using Xunit;
+
+namespace HelloCoreClrApp.Test.WebApi
+{
+    public class WebHostTaskTest
+    {
+        [Fact]
+        public async void RunTestAsync()
+        {
+            Startup.Container = new Container();
+            var conf = A.Fake<IConfiguration>();
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+            await WebHostTask.Run(conf, cts.Token);
+        }
+    }
+}

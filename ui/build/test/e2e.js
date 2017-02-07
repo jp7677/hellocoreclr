ad6eb77923bb9aed4d197dd4aea22c280bcf5b80
Jens Peters <jp7677@gmail.com>
2017-02-07 22:24:45 +0100

    Improve readability.

--- ui/build/test/e2e.js
+++ ui/build/test/e2e.js
@@ -5,22 +5,16 @@ const childprocess = require('child_process')
 
 exports.dep = ['serve:wwwroot']
 exports.fn = (gulp, paths, argv, done) => {
-  let args = ['protractor.conf.js']
-
   let winExt = /^win/.test(process.platform) ? '.cmd' : ''
   let nodeBinDir = path.join('node_modules', '.bin')
   let protractorBin = path.join(nodeBinDir, 'protractor' + winExt)
 
-  let child = childprocess.spawn(protractorBin, args, {
-    stdio: 'inherit'
-  }).on('close', (code) => {
-    if (child) {
-      child.kill()
-    }
-    if (code !== 0) {
-      process.exit(code)
-    } else {
-      done()
-    }
-  })
+  childprocess.spawn(protractorBin, ['protractor.conf.js'], {stdio: 'inherit'})
+    .on('close', (code) => {
+      if (code !== 0) {
+        process.exit(code)
+      } else {
+        done()
+      }
+    })
 }

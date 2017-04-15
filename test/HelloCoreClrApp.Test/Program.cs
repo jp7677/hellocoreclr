0e50c1ec2fad93efad5f2dcbd49cc8b1df2a120d
Jens Peters <jp7677@gmail.com>
2017-04-15 21:35:35 +0200

    Add entry point. Technically not really needed, but fixes a ReSharper warning.

---
+++ test/HelloCoreClrApp.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloCoreClrApp.Test
+{
+    public static class Program
+    {
+        // Entry point for the application.
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please use 'dotnet test' or 'dotnet xunit' to run unit tests.");
+        }
+    }
+}

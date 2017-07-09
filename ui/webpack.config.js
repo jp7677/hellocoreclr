83421925685f8a94fffef8789ddcde8431dc7067
Jens Peters <jp7677@gmail.com>
2017-07-09 23:10:48 +0200

    Restore our splash progress. We need the CommonChunksPlugin for moving nprogress into a bootstrap bundle, otherwise we end up with two nprogress instances. Next to that we need to define the order how the bundles are included in the index.html, splash and bootstrap needs to be loaded before the app bundle. Last we need to load and apply the nprogress stylesheet manually from typescript.javascript, this is somewhat ugly but works since webpack will handle 'require' correctly.

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,30 +1,30 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { AureliaPlugin } = require('aurelia-webpack-plugin');
-const { ProvidePlugin } = require('webpack');
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { AureliaPlugin } = require('aurelia-webpack-plugin')
+const { optimize: { CommonsChunkPlugin }, ProvidePlugin } = require('webpack')
 
-const src = path.resolve(__dirname, 'src');
+const src = path.resolve(__dirname, 'src')
 
 module.exports = {
-  entry: { 
+  entry: {
     app: [ 'aurelia-bootstrapper' ],
     splash: [ 'app/splash' ]
   },
   module: {
-   rules: [
+    rules: [
       { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
       { test: /\.html$/i, use: 'html-loader' },
       { test: /\.css$/i, use: 'css-loader' },
       { test: /\.(png|svg|jpg|gif)$/, use: 'file-loader' },
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' },
-      { test: /\.json$/i, use: 'json-loader' },
+      { test: /\.json$/i, use: 'json-loader' }
     ]
   },
   resolve: {
     extensions: ['.ts', '.js'],
-    modules: [src, 'node_modules'],
+    modules: [src, 'node_modules']
   },
   output: {
     filename: '[name].bundle.js',
@@ -32,10 +32,11 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['wwwroot']),
-    new HtmlWebpackPlugin({ template: 'src/index.ejs' }),
-    new CopyWebpackPlugin([{ from: 'src/favicon.ico', to: 'favicon.ico' }]),
-    new AureliaPlugin({ aureliaApp: 'app/main' }),
     new ProvidePlugin({ '$': 'jquery', 'jQuery': 'jquery' }),
+    new AureliaPlugin({ aureliaApp: 'app/main' }),
+    new CommonsChunkPlugin({ name: 'bootstrap' }),
+    new HtmlWebpackPlugin({ template: 'src/index.ejs', chunks: ['bootstrap', 'splash', 'app'], chunksSortMode: (a, b) => 1 }),
+    new CopyWebpackPlugin([{ from: 'src/favicon.ico', to: 'favicon.ico' }])
   ],
-  devtool : 'source-map'
-};
+  devtool: 'source-map'
+}

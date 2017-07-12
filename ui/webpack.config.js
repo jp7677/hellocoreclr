f36cb3a8aaf8f0693726ba47b0d5f0c0aa4d80e5
Jens Peters <jp7677@gmail.com>
2017-07-12 09:16:13 +0200

    Support specific settings for production build.

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -3,49 +3,51 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { AureliaPlugin } = require('aurelia-webpack-plugin')
 const { optimize: { CommonsChunkPlugin, UglifyJsPlugin }, ProvidePlugin } = require('webpack')
+const noop = require('noop-webpack-plugin')
 
 const src = path.resolve(__dirname, 'src')
 
-module.exports = {
-  entry: {
-    app: [ 'aurelia-bootstrapper' ],
-    splash: [ 'app/splash' ]
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
-      { test: /\.html$/i, use: { loader: 'html-loader', options: { minimize: true } } },
-      { test: /\.css$/i, use: { loader: 'css-loader', options: { minimize: true } } },
-      { test: /\.(png|svg|jpg|gif)$/, use: 'file-loader' },
-      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' },
-      { test: /\.json$/i, use: 'json-loader' }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    modules: [src, 'node_modules']
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'wwwroot'),
-    devtoolModuleFilenameTemplate: './[resource-path]'
-  },
-  plugins: [
-    new CleanWebpackPlugin(['wwwroot']),
-    new ProvidePlugin({ '$': 'jquery', 'jQuery': 'jquery' }),
-    new AureliaPlugin({ aureliaApp: 'app/main' }),
-    new CommonsChunkPlugin({ name: 'bootstrap' }),
-    new UglifyJsPlugin({ comments: false, sourceMap: true }),
-    new HtmlWebpackPlugin({
-      template: 'src/index.ejs',
-      favicon: 'src/favicon.ico',
-      chunks: ['bootstrap', 'splash', 'app'],
-      chunksSortMode: (a, b) => 1,
-      minify: {
-        collapseWhitespace: true,
-        collapseInlineTagWhitespace: true
-      }
-    })
-  ],
-  devtool: 'source-map'
+module.exports = (env) => {
+  const isProduction = env === 'production'
+
+  return {
+    entry: {
+      app: [ 'aurelia-bootstrapper' ],
+      splash: [ 'app/splash' ]
+    },
+    module: {
+      rules: [
+        { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
+        { test: /\.html$/i, use: { loader: 'html-loader', options: { minimize: isProduction } } },
+        { test: /\.css$/i, use: { loader: 'css-loader', options: { minimize: isProduction } } },
+        { test: /\.(png|svg|jpg|gif)$/, use: 'file-loader' },
+        { test: /\.(woff|woff2|eot|ttf|otf)$/, use: 'file-loader' },
+        { test: /\.json$/i, use: 'json-loader' }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      modules: [src, 'node_modules']
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'wwwroot'),
+      devtoolModuleFilenameTemplate: './[resource-path]'
+    },
+    plugins: [
+      new CleanWebpackPlugin(['wwwroot']),
+      new ProvidePlugin({ '$': 'jquery', 'jQuery': 'jquery' }),
+      new AureliaPlugin({ aureliaApp: 'app/main' }),
+      new CommonsChunkPlugin({ name: 'bootstrap' }),
+      isProduction ? new UglifyJsPlugin({ comments: false }) : noop(),
+      new HtmlWebpackPlugin({
+        template: 'src/index.ejs',
+        favicon: 'src/favicon.ico',
+        chunks: ['bootstrap', 'splash', 'app'],
+        chunksSortMode: (a, b) => 1,
+        minify: isProduction ? { collapseWhitespace: true, collapseInlineTagWhitespace: true } : false
+      })
+    ],
+    devtool: !isProduction ? 'source-map' : undefined
+  }
 }

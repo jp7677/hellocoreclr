6d2d6f35d237fbd23290ca9f7f36f5e8a3d699c8
Jens Peters <jp7677@gmail.com>
2018-01-30 21:56:05 +0100

    Update to cucumber-js 4.

--- ui/test-e2e/sayhello.ts
+++ ui/test-e2e/sayhello.ts
@@ -1,23 +1,21 @@
 import { expect } from "chai";
-import { defineSupportCode } from "cucumber";
+import { Given, Then, When } from "cucumber";
 import { browser, by, element } from "protractor";
 
-defineSupportCode(({Given, When, Then}) => {
-    Given(/^I've navigated to the home page$/, async () => {
-        await browser.loadAndWaitForAureliaPage("/");
-    });
+Given(/^I've navigated to the home page$/, async () => {
+    await browser.loadAndWaitForAureliaPage("/");
+});
 
-    Then(/^I should see the say hello page$/, async () => {
-        expect(await browser.getTitle()).to.equal("Say Hello World! | Hello World");
-    });
+Then(/^I should see the say hello page$/, async () => {
+    expect(await browser.getTitle()).to.equal("Say Hello World! | Hello World");
+});
 
-    When(/^I click on last greetings$/, async () => {
-        const waitPromise = browser.waitForRouterComplete();
-        await element(by.css('a[href="#/greetings"]')).click();
-        await waitPromise;
-    });
+When(/^I click on last greetings$/, async () => {
+    const waitPromise = browser.waitForRouterComplete();
+    await element(by.css('a[href="#/greetings"]')).click();
+    await waitPromise;
+});
 
-    Then(/^I should see the last greetings page$/, async () => {
-        expect(await browser.getTitle()).to.equal("Greetings | Hello World");
-    });
+Then(/^I should see the last greetings page$/, async () => {
+    expect(await browser.getTitle()).to.equal("Greetings | Hello World");
 });

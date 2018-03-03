aa718271ac86d003fac422bf60174f88cdca9969
Jens Peters <jp7677@gmail.com>
2018-03-03 21:43:37 +0100

    Make sure that there will really only be one NProgress instance.

--- ui/src/app/statusbar.ts
+++ ui/src/app/statusbar.ts
@@ -1,9 +1,31 @@
 declare function require(name: string): string;
 import * as nprogress from "nprogress";
 
+interface IWindow extends Window {
+    Statusbar: Statusbar;
+}
+
 export class Statusbar {
     public static Start(): void {
-        Statusbar.loadStylesheet();
+        Statusbar.Instance.Start();
+    }
+
+    public static Inc(): void {
+        Statusbar.Instance.Inc();
+    }
+
+    public static Done(): void {
+        Statusbar.Instance.Done();
+    }
+
+    private static get Instance() {
+        // attach this to the window object to make sure that there is really only one.
+        const w = window as IWindow;
+        return w.Statusbar || (w.Statusbar = new this());
+    }
+
+    private Start(): void {
+        this.loadStylesheet();
         nprogress.configure({
             minimum: 0.3,
             showSpinner: false,
@@ -12,20 +34,20 @@ export class Statusbar {
         nprogress.start();
     }
 
-    public static Inc(): void {
-        nprogress.inc(0.6);
-    }
-
-    public static Done(): void {
-        nprogress.done();
-    }
-
-    private static loadStylesheet() {
+    private loadStylesheet() {
         // tslint:disable:no-submodule-imports
         const css = require("nprogress/nprogress.css").toString();
         const style: HTMLStyleElement = document.createElement("style");
         style.type = "text/css";
         style.innerHTML = css;
         document.head.appendChild(style);
+    }
+
+    private Inc(): void {
+        nprogress.inc(0.6);
+    }
+
+    private Done(): void {
+        nprogress.done();
     }
 }

1555b07dd4d094c7c125e7764e9789ccb2c9f1d4
Jens Peters <jp7677@gmail.com>
2018-06-03 22:16:11 +0200

    Use var. Rider finally understands it here ;)

--- src/HelloCoreClrApp/WebApi/Actions/SayHelloWorldAction.cs
+++ src/HelloCoreClrApp/WebApi/Actions/SayHelloWorldAction.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 using HelloCoreClrApp.Data;
 using HelloCoreClrApp.WebApi.Messages;
@@ -20,7 +19,7 @@ namespace HelloCoreClrApp.WebApi.Actions
         {
             Log.Information("Calculating result.");
 
-            Tuple<string, bool> res = Rules.SayHelloWorldRule.Process(name);
+            var res = Rules.SayHelloWorldRule.Process(name);
             if (res.Item2)
                 await SaveGreeting(res.Item1);
 

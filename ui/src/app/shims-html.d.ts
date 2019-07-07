6db5e9c68f0524a511f349c520387dc75c0ded05
Jens Peters <jp7677@gmail.com>
2019-07-07 16:42:15 +0200

    Formatting.

--- ui/src/app/shims-html.d.ts
+++ ui/src/app/shims-html.d.ts
@@ -1,9 +1,10 @@
-declare module '*.html' {
-    import Vue, { ComponentOptions, FunctionalComponentOptions } from 'vue'
+declare module "*.html" {
+    import Vue, { ComponentOptions, FunctionalComponentOptions } from "vue";
+    // tslint:disable-next-line:interface-name
     interface WithRender {
-        <V extends Vue, U extends ComponentOptions<V> | FunctionalComponentOptions>(options: U): U
-        <V extends typeof Vue>(component: V): V
+        <V extends Vue, U extends ComponentOptions<V> | FunctionalComponentOptions>(options: U): U;
+        <V extends typeof Vue>(component: V): V;
     }
-    const withRender: WithRender
-    export default withRender
+    const withRender: WithRender;
+    export default withRender;
 }

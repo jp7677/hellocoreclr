112e4cdfa343b66459de1033648c0326cb1134f0
Jens Peters <jp7677@gmail.com>
2019-07-07 20:30:16 +0200

    Set page titles.

--- ui/src/app/router.ts
+++ ui/src/app/router.ts
@@ -1,21 +1,34 @@
+import Vue from "vue";
 import Router from "vue-router";
 
 import Greetings from "./helloworld/greetings";
 import SayHelloWorld from "./helloworld/sayhelloworld";
 
-export default new Router({
+const applicationTitle = "Hello World";
+const router = new Router({
     routes: [
         {
             component: SayHelloWorld,
+            meta: { title: route => `Say Hello World! | ${applicationTitle}` },
             path: "/"
         },
         {
             component: SayHelloWorld,
+            meta: { title: route => `Say Hello World! | ${applicationTitle}` },
             path: "/sayhelloworld"
         },
         {
             component: Greetings,
+            meta: { title: route => `Greetings | ${applicationTitle}` },
             path: "/greetings"
         }
     ]
 });
+
+router.afterEach((to, from) => {
+    Vue.nextTick(() => {
+      document.title = to.meta.title(to);
+    });
+  });
+
+export default router;

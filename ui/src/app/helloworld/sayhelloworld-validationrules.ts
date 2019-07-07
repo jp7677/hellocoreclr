e1f4c8d0558779d2f02ef1805c36a83487512679
Jens Peters <jp7677@gmail.com>
2019-07-07 15:21:46 +0200

    Fix spacing.

--- ui/src/app/helloworld/sayhelloworld-validationrules.ts
+++ ui/src/app/helloworld/sayhelloworld-validationrules.ts
@@ -3,19 +3,19 @@
 
 export class SayHelloWorldValidationRules {
 
-  // TODO: Setup validation using Vue
+    // TODO: Setup validation using Vue
 
-  /*
-  public setRules(instance: SayHelloWorld) {
+    /*
+    public setRules(instance: SayHelloWorld) {
         // prettier-ignore
-    ValidationRules
-      .ensure((p: SayHelloWorld) => p.inputText)
-        .displayName("Greeting name")
-        .required()
-        .minLength(3)
-        .maxLength(20)
-        .matches(/^[\w\u00C0-\u024f]+$/)
-      .on(instance);
-  }
-  */
+        ValidationRules
+            .ensure((p: SayHelloWorld) => p.inputText)
+                .displayName("Greeting name")
+                .required()
+                .minLength(3)
+                .maxLength(20)
+                .matches(/^[\w\u00C0-\u024f]+$/)
+            .on(instance);
+    }
+    */
 }

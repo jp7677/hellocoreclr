1022626ece675ff7bee7119bb1f4dce8a2fc50a2
Jens Peters <jp7677@gmail.com>
2019-07-29 17:08:47 +0200

    Fix unit tests. We should always use flush-promises, local vue and sync enabled.

--- ui/test/helloworld/greetings.spec.ts
+++ ui/test/helloworld/greetings.spec.ts
@@ -1,17 +1,17 @@
 import { expect } from "chai";
+import flushPromises from "flush-promises";
 
-import { config, RouterLinkStub, shallowMount } from "@vue/test-utils";
+import { config, createLocalVue, RouterLinkStub, shallowMount } from "@vue/test-utils";
 import axios from "axios";
 import moxios from "moxios";
 import { LoggerStub } from "../stubs";
 
 import Greetings from "../../src/app/helloworld/greetings";
 
-import sinon from "sinon";
-
 // tslint:disable:no-unused-expression
-
 describe("Greetings test suite", () => {
+    const Vue = createLocalVue();
+
     beforeEach(() => {
         moxios.install();
         config.mocks.$http = axios;
@@ -23,7 +23,7 @@ describe("Greetings test suite", () => {
         moxios.uninstall();
     });
 
-    it("should handle a valid response", done => {
+    it("should handle a valid response", async () => {
         moxios.stubRequest("greetings/count", { status: 200, response: 5 });
         moxios.stubRequest("greetings", {
             response: [
@@ -33,44 +33,38 @@ describe("Greetings test suite", () => {
             status: 200
         });
 
-        const sut = shallowMount(Greetings).vm;
+        const sut = shallowMount(Greetings, { localVue: Vue, sync: false }).vm;
 
-        moxios.wait(() => {
-            expect(sut.numberOfSavedGreetings).not.to.be.undefined;
-            expect(sut.numberOfSavedGreetings).to.equal(5);
-            expect(sut.savedGreetings).not.to.be.undefined;
-            expect(sut.savedGreetings.length).to.equal(2);
-            done();
-        });
+        await flushPromises();
+        expect(sut.numberOfSavedGreetings).not.to.be.undefined;
+        expect(sut.numberOfSavedGreetings).to.equal(5);
+        expect(sut.savedGreetings).not.to.be.undefined;
+        expect(sut.savedGreetings.length).to.equal(2);
     });
 
-    it("should handle a valid response without content", done => {
+    it("should handle a valid response without content", async () => {
         moxios.stubRequest("greetings/count", { status: 200, response: 0 });
         moxios.stubRequest("greetings", { status: 203 });
 
-        const sut = shallowMount(Greetings).vm;
+        const sut = shallowMount(Greetings, { localVue: Vue, sync: false }).vm;
 
-        moxios.wait(() => {
-            expect(sut.numberOfSavedGreetings).not.to.be.undefined;
-            expect(sut.numberOfSavedGreetings).to.equal(0);
-            expect(sut.savedGreetings).not.to.be.undefined;
-            expect(sut.savedGreetings.length).to.equal(0);
-            done();
-        });
+        await flushPromises();
+        expect(sut.numberOfSavedGreetings).not.to.be.undefined;
+        expect(sut.numberOfSavedGreetings).to.equal(0);
+        expect(sut.savedGreetings).not.to.be.undefined;
+        expect(sut.savedGreetings.length).to.equal(0);
     });
 
-    it("should handle an error response", done => {
+    it("should handle an error response", async () => {
         moxios.stubRequest("greetings/count", { status: 500 });
         moxios.stubRequest("greetings", { status: 500 });
 
-        const sut = shallowMount(Greetings).vm;
+        const sut = shallowMount(Greetings, { localVue: Vue, sync: false }).vm;
 
-        moxios.wait(() => {
-            expect(sut.numberOfSavedGreetings).not.to.be.undefined;
-            expect(sut.numberOfSavedGreetings).to.equal(0);
-            expect(sut.savedGreetings).not.to.be.undefined;
-            expect(sut.savedGreetings.length).equal(0);
-            done();
-        });
+        await flushPromises();
+        expect(sut.numberOfSavedGreetings).not.to.be.undefined;
+        expect(sut.numberOfSavedGreetings).to.equal(0);
+        expect(sut.savedGreetings).not.to.be.undefined;
+        expect(sut.savedGreetings.length).equal(0);
     });
 });
--- ui/test/helloworld/sayhelloworld.spec.ts
+++ ui/test/helloworld/sayhelloworld.spec.ts
@@ -1,13 +1,13 @@
 import { expect } from "chai";
+import flushPromises from "flush-promises";
 
 import { config, createLocalVue, RouterLinkStub, shallowMount } from "@vue/test-utils";
 import axios from "axios";
 import moxios from "moxios";
+import VeeValidate from "vee-validate";
 import { LoggerStub } from "../stubs";
 
 import SayHelloWorld from "../../src/app/helloworld/sayhelloworld";
-
-import VeeValidate from "vee-validate";
 
 // tslint:disable:no-unused-expression
 
@@ -29,7 +29,7 @@ describe("SayHelloWorld test suite", () => {
         moxios.uninstall();
     });
 
-    it("should handle a valid response", async done => {
+    it("should handle a valid response", async () => {
         moxios.stubRequest("sayhelloworld/", { status: 200, response: { greeting: "Hello World!" } });
 
         const sut = shallowMount(SayHelloWorld, { localVue: Vue, sync: false }).vm;
@@ -37,23 +37,20 @@ describe("SayHelloWorld test suite", () => {
 
         await sut.submit();
 
-        moxios.wait(() => {
-            expect(sut.greetingText).to.equal("Hello World!");
-            done();
-        });
+        await flushPromises();
+        expect(sut.greetingText).to.equal("Hello World!");
     });
 
-    it("should handle an error response", async done => {
+    it("should handle an error response", async () => {
         moxios.stubRequest("sayhelloworld/", { status: 500 });
+
         const sut = shallowMount(SayHelloWorld, { localVue: Vue, sync: false }).vm;
         sut.inputText = "Error";
         sut.greetingText = "Hello";
 
         await sut.submit();
 
-        moxios.wait(() => {
-            expect(sut.greetingText).to.equal("");
-            done();
-        });
+        await flushPromises();
+        expect(sut.greetingText).to.equal("");
     });
 });

f391067fd7a3a35b6ac19cdd58e915987126589e
Jens Peters <jp7677@gmail.com>
2020-03-08 09:24:43 +0100

    Add coverage and watch configuration.

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -5,5 +5,10 @@ module.exports = {
   transform: {
     '.+\\.ts$': 'ts-jest',
     '.+\\.html$': 'vue-template-loader-jest'
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!**/*.d.ts'],
+  coverageReporters: ['json', 'text-summary'],
+  coverageDirectory: '../reports/',
+  watchPathIgnorePatterns: ['node_modules']
 }

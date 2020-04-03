bf73c37a4005fa53a5bfa66fa8edb868229a2c52
Jens Peters <jp7677@gmail.com>
2020-04-03 22:38:42 +0200

    Typescript is fast enough.

--- ui/test-e2e/plugins.js
+++ ui/test-e2e/plugins.js
@@ -9,7 +9,7 @@ module.exports = (on) => {
       node: { fs: 'empty', child_process: 'empty', readline: 'empty' },
       module: {
         rules: [
-          { test: /\.ts$/, exclude: [/node_modules/], use: [{ loader: 'ts-loader', options: { transpileOnly: true } }] },
+          { test: /\.ts$/, exclude: [/node_modules/], use: [{ loader: 'ts-loader' }] },
           { test: /\.feature$/, use: [{ loader: 'cypress-cucumber-preprocessor/loader' }] },
           { test: /\.features$/, use: [{ loader: 'cypress-cucumber-preprocessor/lib/featuresLoader' }] }
         ]

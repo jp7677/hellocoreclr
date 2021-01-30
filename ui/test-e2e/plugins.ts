4375cc1827a3f7679a75396f060d1ea9887cb4d0
Jens Peters <jp7677@gmail.com>
2021-01-30 20:41:23 +0100

    Provide path and process. Webpack 5 no longer polyfills those but cucumber-js currently wants them.

--- ui/test-e2e/plugins.ts
+++ ui/test-e2e/plugins.ts
@@ -1,11 +1,15 @@
 import cypressWebpack from "cypress-webpack-preprocessor-v5";
+import { ProvidePlugin } from "webpack";
 
 module.exports = (on): any => {
     on(
         "file:preprocessor",
         cypressWebpack({
             webpackOptions: {
-                resolve: { extensions: [".ts", ".js"] },
+                resolve: {
+                    extensions: [".ts", ".js"],
+                    fallback: { path: require.resolve("path-browserify") },
+                },
                 module: {
                     rules: [
                         { test: /\.ts$/, exclude: [/node_modules/], use: [{ loader: "ts-loader" }] },
@@ -13,6 +17,11 @@ module.exports = (on): any => {
                         { test: /\.features$/, use: [{ loader: "cypress-cucumber-preprocessor/lib/featuresLoader" }] },
                     ],
                 },
+                plugins: [
+                    new ProvidePlugin({
+                        process: "process/browser",
+                    }),
+                ],
             },
         })
     );
